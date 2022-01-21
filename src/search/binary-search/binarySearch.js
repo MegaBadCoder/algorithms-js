89a30289f8a0357d32f3a964c47cc0b175c31c10
i.seliverstov <seliverstoviv@gmail.com>
2022-01-21 14:15:23 +0300

    Added binary search algorithm

--- src/search/binary-search/binarySearch.js
+++ src/search/binary-search/binarySearch.js
@@ -1,3 +1,24 @@
-export default function sum(a, b) {
-  return a + b;
+export default function binarySearch(array, item) {
+  array.sort((a, b) => a - b);
+
+  let low = 0;
+  let high = array.length - 1;
+
+  let mid = Math.floor((low + high) / 2);
+  let quess = array[mid];
+
+  while (low < high) {
+    mid = low + high;
+    quess = array[mid];
+
+    if (quess === item) {
+      return mid;
+    }
+    if (quess > item) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return -1;
 }
